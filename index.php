1e25ce2d57f9e369ffb5a3876586b263d8c491f5
Gianluca-Basili <gianluc.basili@gmail.com>
2023-06-30 17:05:45 +0200

    add form

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php 
     $paragraph = $_POST['paragraph'];
-    $word = $_POST['word']
+    $word = $_POST['word'];
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <?php echo $paragraph." ".$word; ?>
+        <form action="/index.php" method="POST">
+       
+        <textarea name="paragraph" id="" cols="30" rows="10"></textarea>
+        <input type="text" name="word">
+        <input type="button" value="send">
+        </form>
+    </div>
+</body>
+</html>
